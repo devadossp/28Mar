9c2c4c667562bf9570f16f8f299ecef26283605b
Devadoss <devadossp@gmail.com>
2017-03-28 12:18:50 +0530

    initial set of classes

---
+++ ClassA.java
@@ -0,0 +1,12 @@
+package com.test;
+
+Public class ClassA {
+
+	public void method1(){
+		System.out.println("inside method1");
+	}
+
+	public void method2(){
+		System.out.println("method2");
+	}
+}
